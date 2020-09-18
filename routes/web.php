b33a784936e869596a54217c9f6fd4c6789e739b
MatheusRSLima <matheus000@gmail.com>
2020-09-17 23:01:00 -0300

    Atualizando quesitos do banco de dados

--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,25 @@ Route::get('/', function () {
     $helloWorld = 'Hello World';
     return view('welcome', compact('helloWorld'));
 });
+
+Route::get('/model', function () {
+    //$products = \App\ProductConfig::all();
+
+    //$user = new \App\User();
+    /*     $user = \App\User::find(1);
+    $user->name = 'Usuário Teste Editado';
+    $user->save(); */
+    //return \App\User::all();
+    //return \App\User::where('name', 'Darryl Emmerich')->get();
+
+/*     $user = \App\User::create([
+        'name' => 'Nanderson Castro',
+        'email' => 'email100@email.com',
+        'password' => bcrypt('12345678')
+    ]);
+        dd($user); */
+
+    $user = \App\User::find(40);
+
+    return \App\User::all();
+});
